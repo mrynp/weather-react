8d5cd4d9c6e11ecff2209f4f8ab1c733e8abe470
mrynp <110476086+mrynp@users.noreply.github.com>
2024-04-13 01:37:23 +0800

    Add search engine

--- src/App.js
+++ src/App.js
@@ -4,31 +4,76 @@ import axios from "axios";
 function App() {
   // let city = "manila";
   const [location, setLocation] = useState("");
-  const [data, setData] = useState({});
+  const [weather, setWeather] = useState({});
+  // const [data, setData] = useState({});
 
-  let key = "8cac06f7ab6c10287cd06a316ff84a57";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`;
   // axios.get(url).then(showTemperature);
+
+  // const searchLocation = (event) => {
+  //   if (event.key === "Enter") {
+  //     axios.get(url).then((response) => {
+  //       setData(response.data);
+  //       console.log(response.data);
+  //     });
+  //   }
+  // };
+
+  function displayWeather(response) {
+    console.log(response.data.name);
+    // setData(response.data);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let key = "8cac06f7ab6c10287cd06a316ff84a57";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setLocation(event.target.value);
+  }
 
   return (
     <div className="app">
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          value={location}
+          onChange={updateCity}
+          placeholder="Enter location"
+          type="search"
+        />
+      </form>
       <div className="container">
         <div className="top">
           <div className="left">
             <div className="location">
-              <p>Manila</p>
+              <p>{weather.name}</p>
             </div>
             <div className="temp">
-              <h1>30°C</h1>
+              <p>
+                {Math.round(weather.temperature)}
+                <span class="unit">°C</span>
+              </p>
             </div>
           </div>
           <div className="right">
-            <div className="icon"></div>
+            <div className="icon">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
             <p className="details">
-              <span className="description">Moderate rain</span>
+              <span className="description">{weather.description}</span>
               <br />
-              <span className="humidity">Humidity: 87%, </span>
-              <span className="humidity">Wind: 7.2km/h</span>
+              <span className="humidity">Humidity: {weather.humidity}%, </span>
+              <span className="humidity">Wind: {weather.wind}km/h</span>
             </p>
           </div>
         </div>
